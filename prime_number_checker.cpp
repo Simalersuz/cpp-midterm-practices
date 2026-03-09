31dfcd9c5d5e04f27e5063cbaba5d501371f0dae
Şimal Ersuz <simalersuz@gmail.com>
2026-03-09 22:47:35 +0300

    Implement prime number checker in C++
    Add prime number checker using for-loop and break statement

--- prime_number_checker.cpp
+++ prime_number_checker.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int num;
+    int isPrime = 1; 
+
+    cout << "Enter a positive number: ";
+    cin >> num;
+  
+    if (num == 0 || num == 1) {
+        isPrime = 0;
+    }
+
+    for (int i = 2; i < num; i++) {
+        
+        if (num % i == 0) {
+            isPrime = 0; 
+            break; 
+        }
+    }
+
+  
+    if (isPrime == 1) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is not a prime number." << endl;
+    }
+
+    return 0;
+}
